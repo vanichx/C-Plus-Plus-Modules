572acab62dfe36480957e1b690a9ac4d1c0d6a9f
vanichx <vanyapetrunin88@gmail.com>
2024-01-10 15:13:27 +0100

    ex04 finished

---
+++ CPP01/ex04/src/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipetruni <ipetruni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 15:46:21 by ipetruni          #+#    #+#             */
+/*   Updated: 2024/01/10 15:11:04 by ipetruni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "../inc/Colors.h"
+
+void printExitMsg(void) {
+		std::cerr << " ______________________________________________ " << std::endl;
+		std::cerr << "|      Please use the program as follows:      |" << std::endl;
+		std::cerr << "|ˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉˉ|" << std::endl;
+		std::cerr << "|./replace <filename>  <string #1>  <string #2>|" << std::endl;
+		std::cerr << "|______________________________________________|" << std::endl;
+}
+
+void printNoFileMsg(void) {
+	std::cerr << Y " =============================== " << std::endl;
+	std::cerr << R "|  No such file or directory... |" << std::endl;
+	std::cerr << Y " =============================== " R << std::endl;
+}
+
+void searchAndReplace(std::istream& in, std::ofstream& out, std::string const& neededString, std::string const& newString) {
+	
+	size_t index;
+	std::string temp;
+
+	while (std::getline(in, temp)) {
+		index = 0;
+		while ((index = temp.find(neededString, index)) != std::string::npos) {
+			temp.erase(temp.begin() + index, temp.begin() + index + neededString.length());
+			temp.insert(index, newString);
+			index += newString.length();
+		}
+		out << temp;
+	}
+}
+
+int main(int argc, char **argv) {
+	
+	if (argc == 4) {
+		std::ifstream inFile;
+		std::ofstream outFile;
+		std::string inFileName = argv[1];
+		std::string outFileName = inFileName + ".replace";
+		
+		inFile.open(inFileName);
+		if (!inFile) {
+			printNoFileMsg();
+			return (1);	
+		}
+		outFile.open(outFileName);
+		searchAndReplace(inFile, outFile, argv[2], argv[3]);
+		inFile.close();
+		outFile.close();
+	}
+	else {
+		printExitMsg();
+		return (1);
+	}
+	return (0);
+}
