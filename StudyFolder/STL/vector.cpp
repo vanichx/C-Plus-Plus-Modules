b1a4d10ac23f2afb9865a6f098355dc02e0c6376
vanichx <vanyapetrunin88@gmail.com>
2024-04-16 15:33:12 +0200

    CPP08 vector

--- StudyFolder/STL/vector.cpp
+++ StudyFolder/STL/vector.cpp
@@ -6,7 +6,7 @@
 /*   By: ipetruni <ipetruni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 13:07:39 by ipetruni          #+#    #+#             */
-/*   Updated: 2024/04/16 14:13:34 by ipetruni         ###   ########.fr       */
+/*   Updated: 2024/04/16 15:29:09 by ipetruni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,33 @@ using namespace std;
 int main()
 {
 	
-	vector<int> myVector;
+	vector<int> myVector; // init of vector when we dont know the size and all the rest staff
+	// vector<int> myVector(20); // init of vector with spec size 
+	// vector<int> myVector(20, 4); // init of vector with spec size and all the numbers will be initialized to the second parametr 
 	
-	myVector.push_back(2); // .popback() just add one more ellemet to the end of the vector
-	myVector.push_back(44);
-	myVector.push_back(77);
-	myVector.push_back(9);
+	// myVector.reserve(10000); // Increase the capacity of the vector 
 
+	// cout << myVector.empty() << endl; // Just a check that our vector is not empty if return (1 - True , vector doesnot have any elements either return (0 - false , vector has some elements);
+	
+
+	// myVector.resize(20); // change the size of vector 
+
+	// myVector.push_back(2); // .push_back() just add one more ellemet to the end of the vector
+	// myVector.push_back(44);
+	// myVector.push_back(44);
+	// myVector.push_back(44);
+
+
+	cout << "size of vector "<< myVector.size() << endl;
+	cout << "capacity of vector "<< myVector.capacity() << endl;
+
+	// myVector.shrink_to_fit(); // optimize the number of elements , in case you have a big capacity and small size
+
+	// cout << "after" << endl;
+	// cout << "size of vector "<< myVector.size() << endl;
+	// cout << "capacity of vector "<< myVector.capacity() << endl;
+
+	
 	// cout << myVector.size() << endl; // Method .size() returns to us the size of the vector.
 
 	// cout << myVector.at(10) << endl; // Method .at() checks everytime that we are not out of biounds of the vector
@@ -40,9 +60,6 @@ int main()
 	// myVector.pop_back(); // delete th last element in the vector
 
 	// myVector.capacity();
-
-	// cout << "size of vector "<< myVector.size() << endl;
-	cout << "capacity of vector "<< myVector.capacity() << endl;
 
 
 	// for (int i = 0; i < myVector.size(); i++)
